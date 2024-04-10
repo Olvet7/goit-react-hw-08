d7c960c07a14a5748a167e243a717a73d09cf22d
Olvet7 <volya.maslova@gmail.com>
2024-04-10 18:40:40 +0300

    css home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // Home
-import { Box } from "@mui/material";
+import { Box, Link} from "@mui/material";
+// import {Link} from "react-router-dom";
 import RecentActorsIcon from '@mui/icons-material/RecentActors';
 
 export default function Home() {
@@ -12,7 +13,24 @@ export default function Home() {
         This app provides you maintain your contacts. <br />
         Enjoy!
       </h4>
-      <RecentActorsIcon fontSize="large"/>
+      <RecentActorsIcon fontSize="large" sx={{marginBottom: "50px"}}/>
+      <Box sx={{textAlign: "left", margin: "0 auto", padding: "20px 20px" , backgroundColor: "white", color: "var(--accent-color)", width: "200px", borderRadius: "8px"}}>
+     <Box sx={{marginBottom: "20px"}}>
+     <h4>Visit my page:</h4>
+      <Link href="https://github.com/Olvet7">Git</Link>
+     </Box>
+      <h4>My other projects:</h4>
+      <ul>
+        <li>
+        <Link href="https://olvet7.github.io/Code-For-Food-Proj/">Food Boutique</Link>
+        </li>
+        <li>
+        <Link href="<li>
+          <Link>https://olvet7.github.io/Code-For-Food-Proj/</Link>
+        </li>">Minimal</Link>
+        </li>
+      </ul>
+      </Box>
     </Box>
   );
 }
